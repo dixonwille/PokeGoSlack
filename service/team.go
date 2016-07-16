38564eda46497def58a67bc13dc6b7bb363a651d
Will Dixon <dixonwille@hotmail.com>
2016-07-16 16:18:43 -0400

    What is the size of token

--- service/team.go
+++ service/team.go
@@ -3,6 +3,7 @@ package service
 import (
 	"database/sql"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/dixonwille/PokeGoSlack/exception"
 	"github.com/dixonwille/PokeGoSlack/model"
 )
@@ -13,6 +14,8 @@ func InsertTeam(db *sql.DB, body *model.OAuthResp) error {
 	err := db.QueryRow("SELECT TeamName FROM system.Team WHERE TeamId = $1", body.TeamID).Scan(&teamname)
 	switch {
 	case err == sql.ErrNoRows:
+		body.TeamName = body.TeamName[:50]
+		spew.Dump(len(body.AccessToken))
 		_, err = db.Query("INSERT INTO system.Team (TeamId,TeamName,AccessToken) VALUES ($1, $2, $3)", body.TeamID, body.TeamName, body.AccessToken)
 		if err != nil {
 			return err
