8837cb11d5eb6fdf7310fa06867d028e99355153
Will Dixon <dixonwille@hotmail.com>
2016-07-16 13:10:51 -0400

    Forgot controller

---
+++ controller/oauthaccess.go
@@ -0,0 +1,81 @@
+package controller
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/dixonwille/PokeGoSlack/env"
+	"github.com/dixonwille/PokeGoSlack/exception"
+	"github.com/dixonwille/PokeGoSlack/helper"
+	"github.com/dixonwille/PokeGoSlack/model"
+	"github.com/dixonwille/PokeGoSlack/slackapi"
+	"github.com/gorilla/context"
+)
+
+const (
+	oauthAccessEnd string = "/oauth.access"
+)
+
+//OAuthAccess is to ask for permission to connecet to slack
+func OAuthAccess(w http.ResponseWriter, r *http.Request) {
+	code, ok := context.Get(r, env.KeyCode).(string)
+	if !ok {
+		newErr := exception.NewInternalErr(105, "Cannot get code from context")
+		errMsg := model.NewErrorMessage(newErr.Error())
+		newErr.LogError()
+		helper.Write(w, http.StatusInternalServerError, errMsg)
+		return
+	}
+	params := url.Values{}
+	params.Add("client_id", env.ClientID)
+	params.Add("client_secret", env.ClientSecret)
+	params.Add("code", code)
+	api, err := slackapi.NewAPI(oauthAccessEnd, params)
+	if err != nil {
+		errMsg := model.NewErrorMessage(err.Error())
+		env.Logger.Println(err.Error())
+		helper.Write(w, http.StatusInternalServerError, errMsg)
+		return
+	}
+	res, err := http.Get(api.String())
+	if err != nil {
+		errMsg := model.NewErrorMessage(err.Error())
+		env.Logger.Println(err.Error())
+		helper.Write(w, http.StatusInternalServerError, errMsg)
+		return
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		if strings.Contains(res.Header.Get("Content-type"), "text/html") {
+			io.Copy(w, res.Body)
+			return
+		}
+		newErr := exception.NewInternalErr(107, "Something went wrong with slack")
+		errMsg := model.NewErrorMessage(newErr.Error())
+		newErr.LogError()
+		helper.Write(w, http.StatusInternalServerError, errMsg)
+		return
+	}
+	if strings.Contains(res.Header.Get("Content-type"), "application/json") {
+		newErr := exception.NewInternalErr(108, "Slack sent a response back but it should be application/json")
+		errMsg := model.NewErrorMessage(newErr.Error())
+		newErr.LogError()
+		helper.Write(w, http.StatusInternalServerError, errMsg)
+		return
+	}
+	body := new(model.OAuthResp)
+	err = json.NewDecoder(res.Body).Decode(body)
+	if err != nil {
+		errMsg := model.NewErrorMessage(err.Error())
+		env.Logger.Println(err.Error())
+		helper.Write(w, http.StatusInternalServerError, errMsg)
+		return
+	}
+	spew.Dump(body)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Saved"))
+}
