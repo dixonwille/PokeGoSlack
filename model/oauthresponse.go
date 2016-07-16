25bf6105e280cab9057381b843468dbe01d2c4d8
Will Dixon <dixonwille@hotmail.com>
2016-07-16 13:32:16 -0400

    building body out

--- model/oauthresponse.go
+++ model/oauthresponse.go
@@ -4,10 +4,12 @@ import "encoding/json"
 
 //OAuthResp is the response from a handshake to Slack
 type OAuthResp struct {
+	Ok          bool   `json:"ok"`
 	AccessToken string `json:"access_token"`
 	Scope       string `json:"scope"`
 	TeamName    string `json:"team_name"`
 	TeamID      string `json:"team_id"`
+	Error       string `json:"error"`
 }
 
 //ParseOAuthResp parses the response from slack
